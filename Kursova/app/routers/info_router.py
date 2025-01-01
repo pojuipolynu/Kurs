864d7834eae6216d104264ce3cdc04b5888b1069
unknown <valrus1808@gmail.com>
2025-01-02 00:55:19 +0200

    adding titles to respones

--- Kursova/app/routers/info_router.py
+++ Kursova/app/routers/info_router.py
@@ -19,7 +19,7 @@ async def get_albums(album_service: AlbumService = Depends(get_album_service)):
 async def create_album(album_create:AlbumBase, album_service: AlbumService = Depends(get_album_service)):
     return await album_service.create_album(album_create)
 
-@router.get("/albums/songs/{album_id}", response_model=SongsList, status_code=status.HTTP_200_OK)
+@router.get("/albums/songs/{album_id}", status_code=status.HTTP_200_OK)
 async def get_album_songs(album_id:int, album_service: AlbumService = Depends(get_album_service)):
     songs = await album_service.get_album_songs(album_id)
     return SongsList(songs=songs)
@@ -50,7 +50,7 @@ async def get_artists(artist_service: ArtistService = Depends(get_artist_service
 async def create_artist(artist_create:ArtistBase, artist_service: ArtistService = Depends(get_artist_service)):
     return await artist_service.create_artist(artist_create)
 
-@router.get("/artists/songs/{artist_id}", response_model=SongsList, status_code=status.HTTP_200_OK)
+@router.get("/artists/songs/{artist_id}", status_code=status.HTTP_200_OK)
 async def get_artist_songs(artist_id:int, artist_service: ArtistService = Depends(get_artist_service)):
     songs = await artist_service.get_artist_songs(artist_id)
     return SongsList(songs=songs)
--- Kursova/app/routers/user_router.py
+++ Kursova/app/routers/user_router.py
@@ -39,7 +39,7 @@ async def get_one_playlist(playlist_id:int, playlist_service: PlaylistService = 
     playlist = await playlist_service.get_one_playlist(playlist_id)
     return playlist
 
-@router.get("/playlist/{playlist_id}/songs", response_model=SongsList, status_code=status.HTTP_200_OK)
+@router.get("/playlist/{playlist_id}/songs", status_code=status.HTTP_200_OK)
 async def get_playlists_songs(playlist_id:int,  playlist_service: PlaylistService = Depends(get_playlist_service)):
     songs = await playlist_service.get_playlist_songs(playlist_id)
     return SongsList(songs=songs)
