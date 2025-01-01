d0ed9eab1e7bd6a7b80fb73dc11f5f9770e018c1
unknown <valrus1808@gmail.com>
2025-01-02 01:47:44 +0200

    adding titles to respones

--- Kursova/app/repository/playlist_repository.py
+++ Kursova/app/repository/playlist_repository.py
@@ -1,5 +1,5 @@
 from repository.base_repository import BaseRepository
-from db.models import Playlist, List, Song
+from db.models import Playlist, List, Song, Album, Artist
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import select, join
 from sqlalchemy.orm import joinedload
@@ -13,14 +13,38 @@ class PlaylistRepository(BaseRepository):
 
     async def get_playlist_songs(self, playlist_id: int):
         stmt = (
-            select(self.song)
+            select(self.song.id,
+                self.song.title,
+                self.song.artist_id,
+                self.song.fileUrl,
+                self.song.imageUrl,
+                self.song.duration,
+                self.song.album_id,
+                Album.title.label("album"),
+                Artist.name.label("artist"))
             .join(self.list, self.song.id == self.list.song_id)
             .join(self.model, self.list.playlist_id == self.model.id)
+            .join(Album, self.song.album_id == Album.id)
+            .join(Artist, self.song.artist_id == Artist.id)
             .filter(self.model.id == playlist_id)
         )
         result = await self.db.execute(stmt)
-        songs = result.scalars().all()
-        return songs
+        songs = result.mappings().all()  
+
+        return [
+            {
+                "id": song["id"],
+                "title": song["title"],
+                "artist_id": song["artist_id"],
+                "fileUrl": song["fileUrl"],
+                "imageUrl": song["imageUrl"],
+                "duration": song["duration"],
+                "album_id": song["album_id"],
+                "album": song["album"],
+                "artist": song["artist"],
+            }
+            for song in songs
+        ]
     
     async def get_user_playlists(self, user_id: str):
         result = await self.db.execute(select(self.model).filter(self.model.user_id == user_id))
